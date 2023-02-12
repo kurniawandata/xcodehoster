fcb0fbfbde3672a37ecde65f9ca7362b6d4d19ed
Master Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2023-02-12 13:09:40 +0700

    Update aktifkandomain.sh

--- aktifkandomain.sh
+++ aktifkandomain.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#Progammer : Kurniawan. admin@xcodetraining.com. xcode.or.id.
+#Program ini dapat digunakan untuk personal ataupun komersial.
+#X-code Media - xcode.or.id / xcode.co.id
 echo -n "Masukkan nama domain : "
 read domain
 sudo cp support/subdomain.conf /etc/apache2/sites-available/$domain.conf 
