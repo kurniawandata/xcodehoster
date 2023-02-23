e8315b9a6f33b558478bb56a99ca36f92fadde5b
Master Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2023-02-24 00:22:59 +0700

    Create customdomain.sh

---
+++ customdomain.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Progammer : Kurniawan. admin@xcodetraining.com. xcode.or.id.
+#Program ini dapat digunakan untuk personal ataupun komersial.
+#X-code Media - xcode.or.id / xcode.co.id
+echo -n "Masukkan nama domain : "
+read domain
+sed -i "s/xcodehoster.com/$domain/g" /customdomain/tahap1.sh
+sed -i "s/xcodehoster.com/$domain/g" /customdomain/tahap2.sh
+sed -i "s/xcodehoster.com/$domain/g" /customdomain/runtahap1.sh
+sed -i "s/xcodehoster.com/$domain/g" /customdomain/runtahap2.sh
+sudo nano customdomain/aktivasi5.sh
+sudo nano customdomain/aktivasi7.sh
+sudo cp customdomain/tahap1.conf /usr/lib/cgi-bin
+sudo cp customdomain/runtahap1.conf /usr/lib/cgi-bin
+sudo cp customdomain/tahap2.conf /usr/lib/cgi-bin
+sudo cp customdomain/runtahap1.conf /usr/lib/cgi-bin
+sudo cp customdomain/aktivasi5.conf /usr/lib/cgi-bin
+sudo cp customdomain/aktivasi7.conf /usr/lib/cgi-bin
+sudo cp customdomain/acakkode.txt /usr/lib/cgi-bin
+chmod 777 /usr/lib/cgi-bin/*
