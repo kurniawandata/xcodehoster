d967839a8d91da2ed4a64020c2863fe96848d994
Master Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2023-03-05 12:30:49 +0700

    Update customdomain.sh

--- customdomain.sh
+++ customdomain.sh
@@ -10,11 +10,11 @@ sed -i "s/xcodehoster.com/$domain/g" /customdomain/runtahap1.sh
 sed -i "s/xcodehoster.com/$domain/g" /customdomain/runtahap2.sh
 sudo nano customdomain/aktivasi5.sh
 sudo nano customdomain/aktivasi7.sh
-sudo cp customdomain/tahap1.conf /usr/lib/cgi-bin
-sudo cp customdomain/runtahap1.conf /usr/lib/cgi-bin
-sudo cp customdomain/tahap2.conf /usr/lib/cgi-bin
-sudo cp customdomain/runtahap1.conf /usr/lib/cgi-bin
-sudo cp customdomain/aktivasi5.conf /usr/lib/cgi-bin
-sudo cp customdomain/aktivasi7.conf /usr/lib/cgi-bin
+sudo cp customdomain/tahap1.sh /usr/lib/cgi-bin
+sudo cp customdomain/runtahap1.sh /usr/lib/cgi-bin
+sudo cp customdomain/tahap2.sh /usr/lib/cgi-bin
+sudo cp customdomain/runtahap1.sh /usr/lib/cgi-bin
+sudo cp customdomain/aktivasi5.sh /usr/lib/cgi-bin
+sudo cp customdomain/aktivasi7.sh /usr/lib/cgi-bin
 sudo cp customdomain/acakkode.txt /usr/lib/cgi-bin
 chmod 777 /usr/lib/cgi-bin/*
