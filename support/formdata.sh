cf4fa147074ce187b08fbaf2bc471915e1c67266
Master Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2024-11-26 03:01:59 +0700

    Update formdata.sh

--- support/formdata.sh
+++ support/formdata.sh
@@ -19,7 +19,7 @@ cat <<EOT
 <html>
 <head>
     <title>Pendaftaran Xcodehoster</title>
-     <link rel="stylesheet" href="https://xcodehoster.com/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <style>
     { 
         font: bold 16px;
